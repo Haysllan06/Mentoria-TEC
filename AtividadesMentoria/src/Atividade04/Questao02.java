2cef63510e6b16866856ab8d2d72cabf8db3aed0
Davi <haysllan.pereira@aluno.edu.pi.gov.br>
2025-06-04 10:02:30 -0300

    Alterei a idade 2

--- AtividadesMentoria/src/Atividade04/Questao02.java
+++ AtividadesMentoria/src/Atividade04/Questao02.java
@@ -3,7 +3,7 @@ package Atividade04;
 public class Questao02 {
     public static void main(String[] args) {
         int idade1 = 20;
-        int idade2 = 13;
+        int idade2 = 39;
 
         boolean resultado = idade1 > idade2;
         System.out.println("A primeira pessoa é mais velha: " + resultado);
