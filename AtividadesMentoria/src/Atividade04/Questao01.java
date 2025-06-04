f6563a94e252081a790a507228dd304170067536
Davi <haysllan.pereira@aluno.edu.pi.gov.br>
2025-06-04 09:17:16 -0300

    primeiro codigo java

---
+++ AtividadesMentoria/src/Atividade04/Questao01.java
@@ -0,0 +1,16 @@
+package Atividade04;
+
+public class Questao01 {
+    public static void main(String[] args) {
+        double base = 6.0;
+        double altura = 10.0;
+
+        double area_retangulo = base * altura;
+        double perimetro_retangulo = 2 * (base + altura);
+
+        System.out.println("area do retangulo: " + area_retangulo);
+        System.out.println("perimetro do retangulo: " + perimetro_retangulo);
+
+
+    }
+}
