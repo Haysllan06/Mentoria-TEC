b898c0335a34e9b927998aff48ecf103d01dfb8c
Davi <haysllan.pereira@aluno.edu.pi.gov.br>
2025-06-04 10:00:57 -0300

    Meu segundo código Java

---
+++ AtividadesMentoria/src/Atividade04/Questao02.java
@@ -0,0 +1,11 @@
+package Atividade04;
+
+public class Questao02 {
+    public static void main(String[] args) {
+        int idade1 = 20;
+        int idade2 = 13;
+
+        boolean resultado = idade1 > idade2;
+        System.out.println("A primeira pessoa é mais velha: " + resultado);
+    }
+}
